5ca7d2c55d04927b3230bb971b84f8d0e65a79a8
Taporpone <taporpone@gmail.com>
2017-02-28 19:55:23 +0100

    Indents fixed

--- 4_5/src/containers/App.js
+++ 4_5/src/containers/App.js
@@ -21,33 +21,33 @@ class App extends React.Component {
         text:'idk, an hero?'
       }
     ]
-    };
-    this.removeTodo = this.removeTodo.bind(this);
-  }
-  addTodo(val){
-    const todo = {
-      text: val,
-      id: uuid.v4(),
-    };
-    const data = [...this.state.data,todo];
-    this.setState({data});
-  }
-  removeTodo(id){
-    const remainder = this.state.data.filter(todo => todo.id !== id);
-    this.setState({data:remainder});
-  }
-  render(){
-    return (
-      <div className={style.TodoApp}>
-        <div className={style.Title}>
-          <Title todos={this.state.data}/>
-        </div>
-        <div className={style.TodoList}>
-          <TodoList items={this.state.data} onClick={this.removeTodo}/>
-        </div>
+  };
+  this.removeTodo = this.removeTodo.bind(this);
+}
+addTodo(val){
+  const todo = {
+    text: val,
+    id: uuid.v4(),
+  };
+  const data = [...this.state.data,todo];
+  this.setState({data});
+}
+removeTodo(id){
+  const remainder = this.state.data.filter(todo => todo.id !== id);
+  this.setState({data:remainder});
+}
+render(){
+  return (
+    <div className={style.TodoApp}>
+      <div className={style.Title}>
+        <Title todos={this.state.data}/>
       </div>
-    );
-  }
+      <div className={style.TodoList}>
+        <TodoList items={this.state.data} onClick={this.removeTodo}/>
+      </div>
+    </div>
+  );
+}
 }
 
 export default App;
